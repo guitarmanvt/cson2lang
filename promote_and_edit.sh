af8c2b7657887857aa804b0a15460b48e876995b
John Anderson <john@andersoninnovative.com>
2019-06-21 15:28:39 -0400

    Simplify convenience script

--- promote_and_edit.sh
+++ promote_and_edit.sh
@@ -2,4 +2,4 @@
 killall xed
 sudo cp /tmp/hcl.lang /usr/share/gtksourceview-2.0/language-specs/hcl.lang
 sudo cp /tmp/hcl.lang /usr/share/gtksourceview-3.0/language-specs/hcl.lang
-(xed ~/code/ai/sacrificepress.com/setup-ansible.sh ~/code/ai/sacrificepress.com/main.tf &)
+(xed ~/code/ai/sacrificepress.com/main.tf &)
