4a5c7d95650c0c4174d32c48aaf9393380780c90
John Anderson <john@andersoninnovative.com>
2019-06-21 10:08:50 -0400

    Allow self-closing tags, comments, text

--- tests.py
+++ tests.py
@@ -6,27 +6,31 @@ from xmlish import X
 class TestX(unittest.TestCase):
 
     def test_single_tag_returns_no_attributes_or_children(self):
-        self.assertEqual(str(X('body')), '<body>\n</body>')
+        self.assertEqual(str(X('body')), '<body/>')
 
     def test_single_tag_with_attributes_no_children(self):
-        self.assertEqual(str(X('body', shave='haircut')), '<body shave="haircut">\n</body>')
+        self.assertEqual(str(X('body', shave='haircut')),
+                         '<body shave="haircut"/>')
 
     def test_tag_no_attributes_one_child(self):
         x = X('body')
         x.add('beach')
-        self.assertEqual(''.join(x.to_xml_lines(0, 0)), '<body><beach></beach></body>')
+        self.assertEqual(str(x), dedent('''
+            <body>
+              <beach/>
+            </body>
+        ''').strip())
 
     def test_nested_with_attributes(self):
         x = X('vacation', style='family', source='contest')
         x.add('destination', city='Venice', country='Italy')
+        # NOTE: This is a "fluent" usage:
         x.add('activities').add('guided-tour', vehicle='gondola')
         self.assertEqual(str(x), dedent('''
-            <vacation style="family" source="contest">
-              <destination city="Venice" country="Italy">
-              </destination>
+            <vacation source="contest" style="family">
+              <destination city="Venice" country="Italy"/>
               <activities>
-                <guided-tour vehicle="gondola">
-                </guided-tour>
+                <guided-tour vehicle="gondola"/>
               </activities>
             </vacation>
         ''').strip())
--- xmlish.py
+++ xmlish.py
@@ -1,22 +1,37 @@
 """Dirt-simple XML structure building, without the nasties.
 
 Simplifications/limitations:
-- No XML comments are allowed.
-- No internal text is allowed.
+- XML comments are only allowed as siblings to other tags and strictly formatted.
+- Internal Text is allowed, but cannot be intermixed with children or comments.
 - You want a tag with no attributes, you gotta do `X({'tagname': {}})`
 
 Yeah, I know it's limited.
 """
+from textwrap import dedent, indent
 
+class C(object):
+    def __init__(self, comment):
+        self.comment = dedent(comment)
+
+    def to_xml_lines(self, margin=0):
+        output = []
+        spaces = ' ' * margin
+        output.append('{}<!--'.format(spaces))
+        output.append(indent(self.comment, ' ' * margin))
+        output.append('{}-->'.format(spaces))
+        return output
 
 class X(dict):
-    def __init__(self, tagname, **attributes):
+    def __init__(self, tagname, text=None, **attributes):
         self.tagname = tagname
+        self.text = text
         self.attributes = attributes if attributes else dict()
         self.children = []
 
     def add(self, what, **attributes):
-        if type(what) == X:
+        if self.text:
+            raise AttributeError('Node has text and cannot have children.')
+        if type(what) in [X, C]:
             child = what
         else:
             child = X(what, **attributes)
@@ -26,15 +41,19 @@ class X(dict):
     def to_xml_lines(self, margin=0, indent=2):
         output = []
         if self.attributes:
-            kvpairs = ('{}="{}"'.format(attr, val) for attr, val in self.attributes.items())
+            kvpairs = ('{}="{}"'.format(attr, val) for attr, val in sorted(self.attributes.items()))
             formatted_attrs = ' ' + ' '.join(kvpairs)
         else:
             formatted_attrs = ''
         spaces = ' ' * margin
-        output.append('{}<{}{}>'.format(spaces, self.tagname, formatted_attrs))
-        for child in self.children:
-            output.extend(child.to_xml_lines(margin + indent, indent))
-        output.append('{}</{}>'.format(spaces, self.tagname))
+        if self.children or self.text:
+            output.append('{}<{}{}>'.format(spaces, self.tagname, formatted_attrs))
+            for child in self.children:
+                output.extend(child.to_xml_lines(margin + indent, indent))
+            output.append('{}</{}>'.format(spaces, self.tagname))
+        else:
+            # Self-closing tag.
+            output.append('{}<{}{}/>'.format(spaces, self.tagname, formatted_attrs))
         return output
 
     def __str__(self):
